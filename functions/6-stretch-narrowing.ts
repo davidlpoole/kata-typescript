cc8c6a5516d3fb3ef296aaccc051edf753fa4556
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-10-29 19:22:34 +1300

    Complete 6-stretch-narrowing

--- functions/6-stretch-narrowing.ts
+++ functions/6-stretch-narrowing.ts
@@ -1,10 +1,19 @@
 // DOCS - https://www.typescriptlang.org/docs/handbook/2/narrowing.html
 
+type strOrNumOrBool = string | number | boolean
 // getOpposite should return the opposite of the item passed in
 // if the item is a string, return the reversed string
 // if the item is a number, return the negative number
 // if the item is a boolean, return the opposite boolean
-export function getOpposite(): void {}
+export function getOpposite(item: strOrNumOrBool): strOrNumOrBool {
+  if (typeof item === 'string') {
+    return item.split('').reverse().join('')
+  } else if (typeof item === 'number') {
+    return -item
+  } else if (typeof item === 'boolean') {
+    return !item
+  } else return item
+}
 
 // https://www.typescriptlang.org/docs/handbook/2/narrowing.html#the-in-operator-narrowing
 
@@ -24,11 +33,17 @@ interface Improviser {
   act: () => string
 }
 
+type Performer = Musician | Dancer | Improviser
+
 // nextAct will be passed a performer and should call the appropriate method
 // if the performer is a Musician, call play
 // if the performer is a Dancer, call dance
 // if the performer is an Improviser, call act
-export function nextAct(): void {}
+export function nextAct(performer: Performer): void {
+  if ('play' in performer) performer.play()
+  if ('dance' in performer) performer.dance()
+  if ('act' in performer) performer.act()
+}
 
 // https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates
 
@@ -56,20 +71,33 @@ export type Trash = Recycling | 'rubbish'
 
 // isMetal will be passed an item of Recycling
 // it should return true if the item is Metal
-export function isMetal(): void {}
+export function isMetal(item: Recycling): item is Metal {
+  return 'magnetize' in item
+}
 
 // isPaper will be passed an item of Recycling
 // it should return true if the item is Paper
-export function isPaper(): void {}
+export function isPaper(item: Recycling): item is Paper {
+  return 'compost' in item
+}
 
 // isGlass will be passed an item of Recycling
 // it should return true if the item is Glass
-export function isGlass(): void {}
+export function isGlass(item: Recycling): item is Glass {
+  return 'melt' in item
+}
 
 // isRecycling will be passed an item of Trash
 // it should return false if the item is rubbish and true if it is Recycling
-export function isRecycling(item: Trash): void {}
+export function isRecycling(item: Trash): item is Recycling {
+  return (
+    typeof item !== 'string' &&
+    (isGlass(item) || isPaper(item) || isMetal(item))
+  )
+}
 
 // sortRecycling will be passed an array of Trash items
 // and should return an array of Recycling items only
-export function sortRecycling(): void {}
+export function sortRecycling(items: Trash[]): Recycling[] {
+  return items.filter(isRecycling)
+}
