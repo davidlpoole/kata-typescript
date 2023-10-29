793aa61377690ca01b42b4aaaa9a939bdd1f9db5
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-10-29 15:35:28 +1300

    Complete 4-stretch-function-type

--- functions/4-stretch-function-type.ts
+++ functions/4-stretch-function-type.ts
@@ -3,17 +3,27 @@
 // callsFunction should not return anything
 // callsFunction should accept a function as a parameter and call the given function
 // the given function takes no arguments and does not return a value
-export function callsFunction(): void {}
+export function callsFunction(fn: () => void): void {
+  fn()
+}
 
 // wait should not return a value, but accepts a number and a function as parameters
 // it should call the function after the given number of milliseconds
-export function wait(): void {}
+export function wait(num: number, fn: () => void): void {
+  setTimeout(() => fn(), num)
+}
 
 // getIsTypeFn should accept a string as a parameter and return a function
 // the returned function should accept a value (of any type) and return a boolean if
 // the type of the value matches the string passed into getIsTypeFn
-export function getIsTypeFn(): void {}
+export function getIsTypeFn(str: string): (a: unknown) => boolean {
+  return (a) => {
+    return typeof a === str
+  }
+}
 
 // getCapitaliseFn should return a function
 // the returned function should accept a string and return a string with the first letter capitalised
-export function getCapitaliseFn(): void {}
+export function getCapitaliseFn(): (a: string) => string {
+  return (a) => a.charAt(0).toUpperCase() + a.slice(1)
+}
